c949964bb90eefb0b146c8026d86cf911b8eef0a
Madelyn-of-Hell <116776146+Madelyn-of-Hell@users.noreply.github.com>
2024-11-29 09:14:51 +1100

    fixed a few errors and refactored code.

--- src/src.ino
+++ src/src.ino
@@ -1,4 +1,3 @@
-#include <Servo.h>
 #include <math.h>
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
@@ -15,6 +14,8 @@ uint8_t Base_Vertical    = 1;
 uint8_t Elbow_Vertical   = 2;
 uint8_t Wrist_Horizontal = 3;
 
+Adafruit_PWMServoDriver servo = Adafruit_PWMServoDriver();
+
 float Forearm = 2.5; // Remember to update this
 float Aftarm  = 2.5; // Remember to update this
 
@@ -22,26 +23,25 @@ void setup() {
   Serial.begin(115200); // Output to connected computer
   Serial.println("Begin controlling now"); // Idk we may need a more appropriate startup message but that *is* the end goal so 
 
-  pwm.begin(); // Initialise connection to the adafruit servo controller thingy
-  pwm.setOscillatorFrequency(27*pow(10,6)); // Set the oscillator to 27MHz
-  pwm.setPWMFreq(SERVO_FREQ);  // Tells the PWM board the speed to output to the servos
+  servo.begin(); // Initialise connection to the adafruit servo controller thingy
+  servo.setOscillatorFrequency(27*pow(10,6)); // Set the oscillator to 27MHz
+  servo.setPWMFreq(SERVO_FREQ);  // Tells the PWM board the speed to output to the servos
 
   delay(10); // Wait 10ms
 };
-
 
 int moveto(int rad, int deg) {
   rad = constrain(rad, 0, Forearm + Aftarm); // Stops you from going to a location farther away than the robot can reach
   deg = deg%360; // Converts all angles to a number between 1 & 360
 
-  pwm.setPWM(Base_Horizontal, 0, map(deg, 0, 360, SERVOMIN, SERVOMAX)); // Spins the base of the robot to the desired angle
+  servo.setPWM(Base_Horizontal, 0, map(deg, 0, 360, SERVOMIN, SERVOMAX)); // Spins the base of the robot to the desired angle
 
-  float Base_Vert_Angle = acos((pow(rad, 2)+pow(Aftarm, 2) - pow(Forearm))/2*Forearm*rad)); // Gets the angle of the shoulder joint using the law of cosines.
-  pwm.setPWM(Base_Vertical, 0, map(Base_Vert_Angle, 0, 360, SERVOMIN, SERVOMAX)); // Raises the shoulder to the desired angle
+  float Base_Vert_Angle = acos((pow(rad, 2)+(Aftarm, 2) - pow(Forearm,2))/2*Forearm*rad); // Gets the angle of the shoulder joint using the law of cosines.
+  servo.setPWM(Base_Vertical, 0, map(Base_Vert_Angle, 0, 360, SERVOMIN, SERVOMAX)); // Raises the shoulder to the desired angle
 
 
-  float Elbow_Vert_Angle = acos((pow(Forearm, 2) + pow(Aftarm, 2) - pow(rad, 2)) / 2 * Forearm * Aftarm)))); // Gets the angle of the shoulder joint using the law of cosines.
-  pwm.setPWM(Elbow_Vertical, 0, map(Elbow_Vert_Angle, 0, 360, SERVOMIN, SERVOMAX)); // Raises the elbow to the desired angle
+  float Elbow_Vert_Angle = acos((pow(Forearm, 2) + pow(Aftarm, 2) - pow(rad, 2)) / 2 * Forearm * Aftarm); // Gets the angle of the shoulder joint using the law of cosines.
+  servo.setPWM(Elbow_Vertical, 0, map(Elbow_Vert_Angle, 0, 360, SERVOMIN, SERVOMAX)); // Raises the elbow to the desired angle
   return 1;
 };
 
@@ -49,6 +49,9 @@ int wave = 0;
 void loop() {
   int success = moveto(sin(wave), wave%360);
   wave = (wave+1)%360;
+
+  if (Serial.available()) {
+    Serial.print("Keypress:");
+    Serial.println(Serial.read());
+  }
 };
-
-
